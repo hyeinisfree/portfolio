a3d0520e9d00841f039459d6717fb4a78d3dcff4
Hyein Kim <hyeinisfree.dev@gmail.com>
2025-07-09 16:54:40 +0900

    ssss

--- app/(routes)/projects/page.tsx
+++ app/(routes)/projects/page.tsx
@@ -1,37 +1,90 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import matdongsan from '@/public/images/matdongsan.webp';
+import { Button } from "@/components/ui/button";
+import { ProjectCard } from "@/components/ui/ProjectCard";
+import matdongsan from "@/public/images/matdongsan.webp";
+import ssgImage from "@/public/images/sswu.jpg";
 
 export default function Projects() {
   return (
     <div className="flex flex-col justify-center font-clash">
-      <div className="flex">
-        <span>Year</span>
-        <Button>All</Button>
-        <Button>2025</Button>
-        <Button>2024</Button>
-        <Button>2022</Button>
-      </div>
-      <div className="flex">
-        <span>Position</span>
-        <Button>All</Button>
-        <Button>Backend</Button>
-        <Button>Frontend</Button>
-      </div>
-      <div className="flex">
-        <span>Tech Stack</span>
-        <Button>All</Button>
-        <Button>Java/SpringBoot</Button>
-        <Button>Ruby</Button>
-      </div>
-
-      <div className="flex flex-col">
-        <h2 className="text-4xl xl:text-5xl 2xl:text-6xl font-medium mb-4 md:mb-8 xl:mb-14 2xl:mb-20">
-          맛동산
-        </h2>
-        <div className="w-1/4">
-          <Image src={matdongsan} alt="matdongsan"></Image>
+      <h1 className="text-6xl my-4">Projects</h1>
+      <div className="filter">
+        <div className="flex items-center">
+          <span className="w-[100px]">Year</span>
+          <Button variant="outline" size="sm">
+            All
+          </Button>
+          <Button variant="outline" size="sm">
+            2025
+          </Button>
+          <Button variant="outline">2024</Button>
+          <Button variant="outline">2022</Button>
         </div>
+        <div className="flex items-center">
+          <span className="w-[100px]">Position</span>
+          <Button variant="outline">All</Button>
+          <Button variant="outline">Backend</Button>
+          <Button variant="outline">Frontend</Button>
+        </div>
+        <div className="flex items-center">
+          <span className="w-[100px]">Tech Stack</span>
+          <Button variant="outline">All</Button>
+          <Button variant="outline">Java/SpringBoot</Button>
+          <Button variant="outline">Ruby</Button>
+        </div>
+      </div>
+      <div className="items grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <ProjectCard
+          title="맛동산"
+          description="제철 음식 아카이빙/커뮤니티 서비스"
+          startDate="2021/05"
+          endDate="2022/09"
+          thumbnail={matdongsan}
+          position="Backend Developer"
+          techStack={["SpringBoot", "Java", "MySQL", "AWS"]}
+          icon={matdongsan}
+        />
+        <ProjectCard
+          title="Hyein Kim"
+          description="제철 음식 아카이빙/커뮤니티 서비스"
+          startDate="2021/05"
+          endDate="2022/09"
+          thumbnail={matdongsan}
+          position="Backend Developer"
+          techStack={["SpringBoot", "Java", "MySQL", "AWS"]}
+          icon={matdongsan}
+        />
+        <ProjectCard
+          title="수정광산"
+          description="성신여자대학교 커뮤니티 서비스"
+          startDate="2021/05"
+          endDate="2022/09"
+          thumbnail={ssgImage}
+          position="Backend Developer"
+          techStack={["SpringBoot", "Java", "MySQL", "AWS"]}
+          icon={ssgImage}
+        />
+        <ProjectCard
+          title="스토리지"
+          description="성신여자대학교 커뮤니티 서비스"
+          startDate="2021/05"
+          endDate="2022/09"
+          thumbnail={ssgImage}
+          position="Backend Developer"
+          techStack={["SpringBoot", "Java", "MySQL", "AWS"]}
+          award="2021 원티드 '해,커리어' 예선 진출 (상위 50개팀)"
+          icon={ssgImage}
+        />
+        <ProjectCard
+          title="띵-동"
+          description="성신여자대학교 커뮤니티 서비스"
+          startDate="2021/05"
+          endDate="2022/09"
+          thumbnail={ssgImage}
+          position="Backend Developer"
+          techStack={["SpringBoot", "Java", "MySQL", "AWS"]}
+          award="2021 원티드 '해,커리어' 예선 진출 (상위 50개팀)"
+          icon={ssgImage}
+        />
       </div>
     </div>
   );
---
+++ components/ui/ProjectCard.tsx
@@ -0,0 +1,77 @@
+import Image, { StaticImageData } from "next/image";
+import { Badge } from "@/components/ui/badge"; // shadcn/ui의 Badge 사용
+import { TrophyIcon, Calendar, ContactRound } from "lucide-react"; // lucide-react에서 아이콘 사용
+
+export interface ProjectCardProps {
+  title: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  thumbnail: StaticImageData;
+  position: string;
+  techStack: string[];
+  award?: string;
+  icon: StaticImageData;
+}
+
+export const ProjectCard = ({
+  title,
+  description,
+  startDate,
+  endDate,
+  thumbnail,
+  position,
+  techStack,
+  award,
+  icon,
+}: ProjectCardProps) => {
+  return (
+    <div className="flex flex-col w-full rounded-2xl border shadow-sm bg-white overflow-hidden">
+      <div className="aspect-video relative w-full">
+        <Image src={thumbnail} alt={title} fill className="object-cover" />
+      </div>
+      <div className="flex flex-col gap-1.5 p-4 h-full">
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2 text-base md:text-lg xl:text-xl font-medium">
+            <Image
+              src={icon}
+              alt={title}
+              className="w-[16px] h-[16px] obrject-cove"
+            />
+            <span>{title}</span>
+          </div>
+          <p className="text-sm xl:text-base text-gray-600">{description}</p>
+        </div>
+        <div className="flex flex-col gap-0.5 text-xs xl:text-sm">
+          {award && (
+            <div className="flex items-start gap-1 text-yellow-700">
+              <TrophyIcon className="size-3 mt-1" />
+              <p>{award}</p>
+            </div>
+          )}
+          <div className="flex items-start gap-1">
+            <ContactRound className="size-3 mt-1" />
+            <p>{position}</p>
+          </div>
+          <div className="flex items-start gap-1">
+            <Calendar className="size-3 mt-1" />
+            <p>
+              {startDate} → {endDate}
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-1.5 mt-auto">
+          {techStack.map((tech) => (
+            <Badge
+              key={tech}
+              variant="secondary"
+              className="text-[0.7rem] lg:text-[0.72rem] xl:text-xs px-2 py-1"
+            >
+              {tech}
+            </Badge>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
